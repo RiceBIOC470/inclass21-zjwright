471729c48b5956820df0fc6435c692553cd2ddcf
warmflasha <warmflasha@gmail.com>
2017-11-09 10:46:30 -0600

    Wrote inclass21

---
+++ Inclass21.m
@@ -0,0 +1,16 @@
+%In class 21
+
+% 1. Consider the system we discussed in class for a gene which is produced
+% and degraded: dx/dt = k - d*x. Now consider that this gene turns on a
+% second gene y, which also degrades so that its equation is 
+% dy/dt = k2*x - d2*y. Choose some values for k,d,k2, and d2 and run a
+% numerical simulation of this system for the case where both genes start
+% out off ( x(0) = 0 and y(0) = 0). Plot the value of x and y in time and 
+% explain your results. 
+
+% 2. Consider the following model: 
+% dx/dt = y
+% dy/dt = (1-x^2)*y - x
+% write code that simulates this system for several different starting
+% values of x and y. Plot the results and comment on them. 
+
